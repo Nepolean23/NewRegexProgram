9d46e979742833c7f498b71dd1346b4e8eb21d85
Nepolean23 <nlean1996@gmail.com>
2023-01-24 21:12:20 +0530

    [NEPOLEAN]-Regex Code for Checking The FirstName

---
+++ Regex/FirstName.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProgram
+{
+    public class RegexFirstname
+    {
+        public string RegexOptions = "^[A-Z]{1}[a-z]+$";
+
+        public void FirstName()
+        {
+            Console.WriteLine("Enter your Firstname:");
+            string firstname = Console.ReadLine();
+
+            Regex rg = new Regex(RegexOptions);
+
+
+            if (rg.IsMatch(firstname))
+            {
+                Console.WriteLine("The firstname is valid");
+
+            }
+            else
+            {
+                Console.WriteLine("The firstname is invalid");
+            }
+        }
+    }
+}
